d6d5826faf250279827ba6c7d9a368af67a5d049
Marcelo Rodrigues <marcelo_rod_@hotmail.com>
2017-10-07 15:46:41 -0300

    resolução de bug no build

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/dist'));
 
+app.get('*',function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
 app.listen(app.get('port'), function() {
   console.log('NgArchitect is running on port ', app.get('port'));
 });
